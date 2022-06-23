4330372e4143d3e2ed911669769e36f02335a41b
Liam McCarthy <ltmccarthy9@gmail.com>
2022-06-23 13:44:07 -0500

    game finished

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ var wins = 0;
 var losses = 0;
 var ties = 0;
 var RPS = ['R', 'P', 'S']
-window.alert("wins: " + wins + "\nlosses: " + losses + "\nties: " + ties)
-
 
 while (play){
     var randNum = Math.floor(Math.random()*3)
@@ -29,5 +27,30 @@ while (play){
             window.alert("wins: " + wins + "\nlosses: " + losses + "\nties: " + ties)
             play = confirm("Do you want to play again?")
         }
+    } else if (userAnswer == 'P'){
+        if (computerAnswer == 'R') {
+            window.alert("You won!")
+            wins++;
+            window.alert("wins: " + wins + "\nlosses: " + losses + "\nties: " + ties)
+            play = confirm("Do you want to play again?")
+        } else {
+            window.alert("Computer won.")
+            losses++;
+            window.alert("wins: " + wins + "\nlosses: " + losses + "\nties: " + ties)
+            play = confirm("Do you want to play again?")
+        }
+    } else if (userAnswer == 'S'){
+        if (computerAnswer == 'P') {
+            window.alert("You won!")
+            wins++;
+            window.alert("wins: " + wins + "\nlosses: " + losses + "\nties: " + ties)
+            play = confirm("Do you want to play again?")
+        } else {
+            window.alert("Computer won.")
+            losses++;
+            window.alert("wins: " + wins + "\nlosses: " + losses + "\nties: " + ties)
+            play = confirm("Do you want to play again?")
+        }
     }
+
 }
